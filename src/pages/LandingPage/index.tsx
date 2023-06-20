89af0bb0c61d6fe202a5f58a801751ffec4c73e4
eduardohenri22 <eduparaujo@outlook.com>
2023-06-20 16:49:21 -0300

    add LandingPage

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,9 +1,18 @@
 import { StyledLanding } from "./styled";
+import { Link } from "react-router-dom";
+import LogoPMESP from "../../assets/logoPMESP.png";
 
 const LandingPage = () => {
   return (
-    <StyledLanding>
-      <h1>Landing Page</h1>
+    <StyledLanding className="background">
+      <h1 className="h1LandingPage">Policia Militar Do Estado de São Paulo</h1>
+
+      <Link to="/home">
+        <button>Iniciar</button>
+      </Link>
+      <div className="divImg">
+        <img src={LogoPMESP} alt="logo" />
+      </div>
     </StyledLanding>
   );
 };
--- src/pages/LandingPage/styled.ts
+++ src/pages/LandingPage/styled.ts
@@ -6,9 +6,80 @@ export const StyledLanding = styled.div`
   background-position: center;
   width: 100vw;
   height: 100vh;
+  gap: 50px;
 
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  animation: 2s puff-in-center;
+
+  .divImg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  button {
+    padding: 20px;
+    border-radius: 8px;
+    border: transparent;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: red;
+    width: 100px;
+    transition: 0.5s;
+    color: white;
+  }
+
+  img {
+    width: 250px;
+  }
+
+  .h1LandingPage {
+    font-size: 28px;
+    font-weight: 700;
+    letter-spacing: 1rem;
+  }
+
+  .puff-in-center {
+    -webkit-animation: puff-in-center 0.7s cubic-bezier(0.47, 0, 0.745, 0.715)
+      both;
+    animation: puff-in-center 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) both;
+  }
+
+  @-webkit-keyframes puff-in-center {
+    0% {
+      -webkit-transform: scale(2);
+      transform: scale(2);
+      -webkit-filter: blur(4px);
+      filter: blur(4px);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      opacity: 1;
+    }
+  }
+  @keyframes puff-in-center {
+    0% {
+      -webkit-transform: scale(2);
+      transform: scale(2);
+      -webkit-filter: blur(4px);
+      filter: blur(4px);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      opacity: 1;
+    }
+  }
 `;
